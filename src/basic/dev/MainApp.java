2e5d5d514d6aec579f5d6b3b54e327f97ccffee5
ChanAn2004 <120091647+ChanAn2004@users.noreply.github.com>
2022-12-08 16:33:43 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,44 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//thong tin ca nhan
+		String ten = "VO CHAN AN";
+		String MSV = "2260189";
+		String CMND = "01324196498";
+		String Lop = "22CDTH11";
+		String Tentruong = "HUEIC";
+		String Dtb = "5,0";
+		System.out.println(ten);
+		System.out.println(MSV);
+		System.out.println(CMND);
+		System.out.println(Lop);
+		System.out.println(Tentruong);
+		System.out.println(Dtb);
+		
+		//thong tin cong dan
+		String Ten = "VO CHAN AN";
+		String Diachi = "Hue";
+		String SDT = "016549678615";
+		String Luong = "3 trieu";
+		System.out.println(Ten);
+		System.out.println(Diachi);
+		System.out.println(SDT);
+		System.out.println(Luong);
+		
+		//thong tin mot chiec xe 
+		String TenXe = "Ferri";
+		String Hang = "Sieu xe";
+		String Gia = "30 ty";
+		String MauSon = "RED NEON";
+		System.out.println(TenXe);
+		System.out.println(Hang);
+		System.out.println(Gia);
+		System.out.println(MauSon);
+
+
+	}
+
+}
